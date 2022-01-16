2a4ab2dec2640650c9c3fea98716d4de87d32532
kimhan0421 <kimhan0421@naver.com>
2022-01-16 17:58:03 +0900

    feat: color

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const Div = styled.div`
 	font-size: 20px;
-	color: blue;
+	color: ${(props) => props.theme.colors.secondary.orangeD1};
 `;
 
 function Home() {
