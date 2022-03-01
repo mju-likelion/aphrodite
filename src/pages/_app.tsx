9389f4217ad81b0cd6f1ebb60530589fbd805a34
ndaemy <yuyaebean@gmail.com>
2022-03-02 02:44:32 +0900

    feat: 채널톡 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,7 @@ import Header from "@components/Header";
 import Footer from "@components/Footer";
 import Notice from "@components/Notice";
 import { useRouter } from "next/router";
+import Script from "next/script";
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -28,6 +29,51 @@ function MyApp({ Component, pageProps }: AppProps) {
         <GlobalStyle />
         <AppContainer>
           <Header />
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function() {
+                    var w = window;
+                    if (w.ChannelIO) {
+                    return (window.console.error || window.console.log || function(){})('ChannelIO script included twice.');
+                  }
+                  var ch = function() {
+                    ch.c(arguments);
+                  };
+                  ch.q = [];
+                  ch.c = function(args) {
+                    ch.q.push(args);
+                  };
+                  w.ChannelIO = ch;
+                  function l() {
+                    if (w.ChannelIOInitialized) {
+                      return;
+                    }
+                    w.ChannelIOInitialized = true;
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript';
+                    s.async = true;
+                    s.src = 'https://cdn.channel.io/plugin/ch-plugin-web.js';
+                    s.charset = 'UTF-8';
+                    var x = document.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(s, x);
+                  }
+                  if (document.readyState === 'complete') {
+                    l();
+                  } else if (window.attachEvent) {
+                    window.attachEvent('onload', l);
+                  } else {
+                    window.addEventListener('DOMContentLoaded', l, false);
+                    window.addEventListener('load', l, false);
+                  }
+                })();
+                ChannelIO('boot', {
+                  "pluginKey": "${process.env.NEXT_PUBLIC_CHANNEL_IO_PLUGIN_KEY}"
+                });
+              `,
+            }}
+          />
           <Component {...pageProps} />
           <Footer />
           <Notice />
